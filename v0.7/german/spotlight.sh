839d8669175bdd0a6285fcd047aaf94431eb497a
Sandro Athaide Coelho <sandro@debian-xps>
2016-01-17 13:06:34 -0500

    Adjusting memory

--- v0.7/german/spotlight.sh
+++ v0.7/german/spotlight.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Dfile.encoding=UTF-8 -jar -jar /opt/spotlight/dbpedia-spotlight-0.7.jar /opt/spotlight/de  http://localhost:80/rest
+java -Dfile.encoding=UTF-8 -Xms6G -Xmx6G -jar -jar /opt/spotlight/dbpedia-spotlight-0.7.jar /opt/spotlight/de  http://localhost:80/rest
