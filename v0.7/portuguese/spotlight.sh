61b9655a02826991893e3965ed612df3d38b6fd3
Sandro Athaide Coelho <sandroacoelho@gmail.com>
2016-02-01 05:20:30 -0200

    Fix script path

--- v0.7/portuguese/spotlight.sh
+++ v0.7/portuguese/spotlight.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Dfile.encoding=UTF-8 -Xms3G -Xmx3G -jar -jar /opt/spotlight/dbpedia-spotlight-0.7.jar /opt/spotlight/pt  http://localhost:80/rest
+java -Dfile.encoding=UTF-8 -Xms3G -Xmx3G -jar -jar /opt/spotlight/dbpedia-spotlight-latest.jar /opt/spotlight/pt  http://localhost:80/rest
--- v0.7/spanish/spotlight.sh
+++ v0.7/spanish/spotlight.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Dfile.encoding=UTF-8 -Xms4G -Xmx4G -jar -jar /opt/spotlight/dbpedia-spotlight-0.7.jar /opt/spotlight/es  http://localhost:80/rest
+java -Dfile.encoding=UTF-8 -Xms4G -Xmx4G -jar -jar /opt/spotlight/dbpedia-spotlight-latest.jar /opt/spotlight/es  http://localhost:80/rest
