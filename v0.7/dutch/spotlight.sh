99f46ad7f230f6a55949fe47e4b29cd7ad33ef7a
Sandro <sandroacoelho@gmail.com>
2017-01-15 11:19:34 -0200

    Update spotlight.sh

--- v0.7/dutch/spotlight.sh
+++ v0.7/dutch/spotlight.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Dfile.encoding=UTF-8  -Xmx8G -jar /opt/spotlight/dbpedia-spotlight-0.7.jar /opt/spotlight/da  http://localhost:80/rest
+java -Dfile.encoding=UTF-8  -Xmx8G -jar /opt/spotlight/dbpedia-spotlight-0.7.jar /opt/spotlight/nl  http://localhost:80/rest
