62aebe0f855b78b6052045c9921d28d883a234df
israel <israel.rodrigues@afferolab.com.br>
2017-06-08 14:45:13 -0300

    Fixing wrong Spotlight version

--- v1.0/english/spotlight.sh
+++ v1.0/english/spotlight.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Dfile.encoding=UTF-8 -Xmx20G -jar /opt/spotlight/dbpedia-spotlight-0.8-beta.jar /opt/spotlight/en  http://localhost:80/rest
+java -Dfile.encoding=UTF-8 -Xmx20G -jar /opt/spotlight/dbpedia-spotlight-1.0.0.jar /opt/spotlight/en  http://localhost:80/rest
--- v1.0/italian/spotlight.sh
+++ v1.0/italian/spotlight.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Dfile.encoding=UTF-8 -Xmx8G -jar /opt/spotlight/dbpedia-spotlight-0.8-beta.jar /opt/spotlight/it  http://localhost:80/rest
+java -Dfile.encoding=UTF-8 -Xmx8G -jar /opt/spotlight/dbpedia-spotlight-1.0.0.jar /opt/spotlight/it  http://localhost:80/rest
