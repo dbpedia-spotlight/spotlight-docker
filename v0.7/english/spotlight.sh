187197de5b56d20fae01c6271e29276d360134e1
sandroacoelho@gmail.com <sandroacoelho@gmail.com>
2017-06-13 11:07:26 -0300

    Fixing path

--- v0.7/english/spotlight.sh
+++ v0.7/english/spotlight.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Dfile.encoding=UTF-8 -Xmx20G -jar /opt/spotlight/dbpedia-spotlight-0.7.jar /opt/spotlight//opt/spotlight/en_2+2  http://localhost:80/rest
+java -Dfile.encoding=UTF-8 -Xmx20G -jar /opt/spotlight/dbpedia-spotlight-0.7.jar /opt/spotlight/en_2+2  http://localhost:80/rest
