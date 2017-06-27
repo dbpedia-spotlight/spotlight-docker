b12d1016b53c579143aaa5109c55eb274d49caa2
sandroacoelho@gmail.com <sandroacoelho@gmail.com>
2017-06-27 12:02:34 -0300

    Changing jar name

--- nightly-build/hungarian/spotlight.sh
+++ nightly-build/hungarian/spotlight.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Dfile.encoding=UTF-8 -Xmx8G -jar /opt/spotlight/dbpedia-spotlight-1.0.0.jar /opt/spotlight/hu  http://0.0.0.0:80/rest
+java -Dfile.encoding=UTF-8 -Xmx8G -jar /opt/spotlight/dbpedia-spotlight-nightly-build.jar /opt/spotlight/hu  http://0.0.0.0:80/rest
