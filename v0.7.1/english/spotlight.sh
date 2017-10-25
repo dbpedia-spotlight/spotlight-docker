e5736f676be707cc6b6d01668c803245f01c40ae
Sandro <sandroacoelho@gmail.com>
2017-10-25 15:03:12 -0200

    Update spotlight.sh

--- v0.7.1/english/spotlight.sh
+++ v0.7.1/english/spotlight.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Dfile.encoding=UTF-8 -Xmx20G -jar /opt/spotlight/dbpedia-spotlight-0.7.1.jar /opt/spotlight/en_2+2  http://0.0.0.0:80/rest
+java -Dfile.encoding=UTF-8 -Xmx10G -jar /opt/spotlight/dbpedia-spotlight-0.7.1.jar /opt/spotlight/en_2+2  http://0.0.0.0:80/rest
