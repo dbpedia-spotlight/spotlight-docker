92e01cfb5df006c50f5102944c377082baa535ad
Sandro <sandroacoelho@gmail.com>
2018-02-18 15:45:36 -0300

    Update spotlight.sh

--- nightly-build/english/spotlight.sh
+++ nightly-build/english/spotlight.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Dfile.encoding=UTF-8 -Xmx10G -jar /opt/spotlight/dbpedia-spotlight-nightly-build.jar /opt/spotlight/en_2+2  http://0.0.0.0:80/rest
+java -Dfile.encoding=UTF-8 -Xmx10G -jar /opt/spotlight/dbpedia-spotlight-nightly-build.jar /opt/spotlight/en  http://0.0.0.0:80/rest
